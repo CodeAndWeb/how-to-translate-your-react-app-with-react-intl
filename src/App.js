665ef9e32935cb5df0a44a32ec279951a0698270
Andreas Loew <andreas@codeandweb.com>
2022-09-26 10:42:02 +0200

    Added react-intl and some demo messages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {FormattedMessage} from "react-intl";
+import React from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const richText = {
+        code: chunks => <code>{chunks}</code>,
+        b: chunks => <b>{chunks}</b>,
+        it: chunks => <it>{chunks}</it>,
+        em: chunks => <em>{chunks}</em>,
+        strong: chunks => <strong>{chunks}</strong>,
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    <FormattedMessage id="app.text"
+                                      defaultMessage="Edit <code>src/App.js</code> and save to reload. Now with <b>{what}</b>!"
+                                      description="Welcome header on app main page"
+                                      values={{
+                                          what: 'react-intl',
+                                          ...richText
+                                      }}
+                    />
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    <FormattedMessage id="app.learn-react-link"
+                                      defaultMessage="Learn React"
+                                      description="Link on react page"/>
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {IntlProvider} from "react-intl";
+
+// polyfills - optional
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/de'; // Add locale data for your supported languages
+import '@formatjs/intl-relativetimeformat/polyfill';
+import '@formatjs/intl-relativetimeformat/locale-data/de'; // Add locale data for your supported languages
+
+import messages_de from "./translations/de.json";
+import messages_fr from "./translations/fr.json";
+
+const messages = {
+    'de': messages_de,
+    'fr': messages_fr
+};
+
+const language = "xx" ; //navigator.language.split(/[-_]/)[0];  // language without region code
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <IntlProvider locale={navigator.language} messages={messages[language]}>
+            <App/>
+        </IntlProvider>
+    </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
